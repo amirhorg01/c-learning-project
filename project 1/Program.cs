978ecf4aaae109baea84c5c9cbebd96ec961718e
lightning <lightning@LIGHTNING>
2026-07-17 17:02:53 +0330

    Add project files.

---
+++ project 1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("please enter your first name");
+            //string user_firstname = Console.ReadLine();
+            //Console.WriteLine($"here you go , your first name in capital is {user_firstname.ToUpper()}");
+            Console.WriteLine("please enter your first name");
+            string firsName = Console.ReadLine();
+            Console.WriteLine("please enter your last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("please enter your phone number");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("please enter your code meli");
+            string codeMeli = Console.ReadLine();
+            Console.WriteLine("please enter your card number");
+            string cardNumber = Console.ReadLine();
+
+            Console.WriteLine("\nall done");
+        }
+    }
+}
