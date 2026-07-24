c81d702e25981e1cbe4df4d17b135035ca2104a4
lightning <lightning@LIGHTNING>
2026-07-24 16:56:57 +0330

    tamrin class

---
+++ tamrin_temp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace tamrin_temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your number");
+            if (!long.TryParse(Console.ReadLine(), out long num))
+            {
+                Console.WriteLine("invalid number");
+            }
+            else
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    num = num * num;
+                }
+             Console.WriteLine(num);
+            }
+        }
+    }
+}
